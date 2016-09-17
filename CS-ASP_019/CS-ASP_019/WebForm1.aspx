dac5ff25f49d743d61e0aaaa8aa544af57ca26be
Trey August <eskridgetrey@yahoo.com>
2016-09-17 12:50:19 -0500

    CS-ASP_019

---
+++ CS-ASP_019/CS-ASP_019/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CS_ASP_019.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Loan Application Form<br />
+        <br />
+        Name:
+        <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Phone Number:
+        <asp:TextBox ID="phoneTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Social Secuirty Number:
+        <asp:TextBox ID="ssTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        Loan Origination Date:<br />
+        <asp:Calendar ID="loanDateCalendar" runat="server"></asp:Calendar>
+        <br />
+        Salary: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="submitButton" runat="server" Height="23px" Text="OK" Width="39px" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP_019/CS-ASP_019/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_019
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            // string result = String.Format("Thank you, {0}, for your business", nameTextBox.Text);
+
+            int phone = int.Parse(phoneTextBox.Text);
+            // string result = String.Format("Thank you, {0}, for your business, Your Social Security code is: {1:000-00-0000} ", nameTextBox.Text, ss);
+
+
+            string result = String.Format("Thank you, {0}, for your business. " + 
+                "<br /> Your Social Security code is: {1:000-00-0000} " +
+                "<br />Phone: {2: (000) 000-0000}" +
+                "<br /> Loan Date: {3:ddd -- d, M, yy}",
+                nameTextBox.Text, 
+                ssTextBox,
+                phone,
+                loanDateCalendar.SelectedDate,
+                );
+
+
+            resultLabel.Text = result;
+        }
+    }
+}
